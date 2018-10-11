969bb5632962e5c36638d7df23bcad004165cdd1
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2018-10-11 13:18:17 +0000

    Update __manifest__.py

--- ees_node_website/__manifest__.py
+++ ees_node_website/__manifest__.py
@@ -11,7 +11,7 @@ Adds backadmin views, and tweaks to native website module
 	
 Made with love.
     """,
-    'depends': ['calendar','website','website_blog','ees_odoo_node'],
+    'depends': ['calendar','utm','website','website_blog','ees_odoo_node'],
     'data': ['views/ees_node_website.xml'],
     'installable': True,
     'application': True,
